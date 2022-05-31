32fdd221925528a0ffad4a9e6b24b624cf209465
9hero <rngnlah@naver.com>
2022-05-31 14:52:38 +0900

    FEAT: 프론트와 협력을 위한 Cors 허가

--- travelcare/src/main/java/com/healthtrip/travelcare/TravelcareApplication.java
+++ travelcare/src/main/java/com/healthtrip/travelcare/TravelcareApplication.java
@@ -3,7 +3,10 @@ package com.healthtrip.travelcare;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -13,4 +16,13 @@ public class TravelcareApplication {
 		SpringApplication.run(TravelcareApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+			}
+		};
+	}
 }
